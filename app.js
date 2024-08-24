727b0ae5eb53b566a03bfdb0e3580a6590c53c41
Titop521 <titop1031@gmail.com>
2024-08-23 19:18:49 -0500

    actualizacion Css, HTML, JS

---
+++ app.js
@@ -0,0 +1,21 @@
+/* variables
+
+const botonEncriptar = document.querySelector(".contenedor_botones_encriptar")
+const textoEncriptar = document.querySelector(".contenedor1_cajatexto")
+
+//const textoEncriptado = document.getElementsByClassName("contenedor2_derecha_evaluar")
+
+/*botonEncriptar.addEventListener("click", e=> 
+    {e.preventDefault();
+        let textoaEncriptar = textoEncriptar.value;
+        console.log(textoaEncriptar);
+    })
+
+
+function encriptar(){
+    let textArea = document.getElementsByClassName (".contenedor1_cajatexto").value;
+    alert(textArea);
+}
+
+*/
+
